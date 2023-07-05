0f8ec914ec264fa2b5a37bd75677f61f5d1c657f
Jevin Sweval <jevinsweval@gmail.com>
2023-07-05 15:59:01 -0400

    build fixes and remove my idevicecrashgrabber submodule since idevicecrashreport now exists

--- build.sh
+++ build.sh
@@ -29,6 +29,7 @@ repo_urls=(
 # brew install --build-bottle libusb
 
 export ITOOLS_PREFIX=${PWD}/prefix
+rm -rf ${ITOOLS_PREFIX}
 mkdir -p ${ITOOLS_PREFIX}/include ${ITOOLS_PREFIX}/lib ${ITOOLS_PREFIX}/lib/pkgconfig
 
 # ln -fs $(greadlink -f wrappers/static-wrap.pl) wrappers/static-clang
@@ -38,12 +39,16 @@ mkdir -p ${ITOOLS_PREFIX}/include ${ITOOLS_PREFIX}/lib ${ITOOLS_PREFIX}/lib/pkgc
 
 BROOT=$(brew --prefix)/opt
 
+brew update
+brew install libtool autoconf automake pkgconfig libxml2 libzip xz zlib libusb gnutls libtasn1 curl mbedtls
+
 # export CC=static-clang
 # export CXX=static-clang++
-export CFLAGS="-O0 -g -I ${ITOOLS_PREFIX}/include"
+export CFLAGS="-O0 -g -I${ITOOLS_PREFIX}/include"
 export CXXFLAGS="${CFLAGS}"
+export LDFLAGS="-L${ITOOLS_PREFIX}/lib"
 # export LDFLAGS="-all-static -mmacosx-version-min=10.7"
-export PKG_CONFIG_PATH="${ITOOLS_PREFIX}/lib/pkgconfig:${BROOT}/libusb/lib/pkgconfig:${BROOT}/libxml2/lib/pkgconfig:${BROOT}/libzip/lib/pkgconfig:${BROOT}/zlib/lib/pkgconfig:${BROOT}/curl/lib/pkgconfig:${BROOT}/openssl@1.1/lib/pkgconfig:${BROOT}/libtasn1/lib/pkgconfig:${BROOT}/gnutls/lib/pkgconfig:${BROOT}/curl/lib/pkgconfig:${BROOT}/lib/pkgconfig"
+export PKG_CONFIG_PATH="${ITOOLS_PREFIX}/lib/pkgconfig:${BROOT}/libusb/lib/pkgconfig:${BROOT}/libxml2/lib/pkgconfig:${BROOT}/libzip/lib/pkgconfig:${BROOT}/xz/lib/pkgconfig:${BROOT}/zlib/lib/pkgconfig:${BROOT}/curl/lib/pkgconfig:${BROOT}/mbedtls/lib/pkgconfig:${BROOT}/openssl@3/lib/pkgconfig:${BROOT}/libtasn1/lib/pkgconfig:${BROOT}/gnutls/lib/pkgconfig:${BROOT}/lib/pkgconfig"
 
 echo CC: ${CC:-NOT_SET}
 echo CXX: ${CXX:-NOT_SET}
@@ -51,6 +56,8 @@ echo CFLAGS: ${CFLAGS:-NOT_SET}
 echo CXXFLAGS: ${CXXFLAGS:-NOT_SET}
 echo LDFLAGS: ${LDFLAGS:-NOT_SET}
 echo PKG_CONFIG_PATH: ${PKG_CONFIG_PATH:-NOT_SET}
+
+NPROC=$(nproc)
 
 mkdir -p src
 pushd src
@@ -64,14 +71,14 @@ pushd src
 			git clean -fdx
 			git pull
 			NOCONFIGURE=1 ./autogen.sh
-			configure_flags=''
+			configure_flags='--enable-debug'
 			if [[ "${name}" = "libplist" ]]; then
-				configure_flags='--enable-debug --without-cython'
+				configure_flags='--without-cython'
 			elif [[ "${name}" = "libimobiledevice" ]]; then
-				configure_flags='--enable-debug-code --without-cython'
+				configure_flags='--without-cython'
 			fi
 			./configure --prefix=${ITOOLS_PREFIX} ${=configure_flags}
-			make -j8
+			make -j $(NPROC)
 			make install
 		popd
 	done
